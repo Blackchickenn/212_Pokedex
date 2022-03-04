5b73f5b94b09e4b39a7e96304f30703146e4eb7d
Blackchickenn <marcel.madaj@gmail.com>
2022-03-04 09:03:32 +0100

    Project Pokedex Grid+API finished

--- main.js
+++ main.js
@@ -1,24 +1,33 @@
-async function getPokemon(){
+async function getAllPokemon(){
     let response = await fetch ("pokemon.json")
     let data = await response.json()
     // console.log(data)
     return data
 }
 
+function getPokemonHtml(samplePokemon){
+    return `
+    <div class="a-pokemon">
+        <div class="a-pokemon-id">${samplePokemon.id}</div>
+        <div class="a-pokemon-name">${samplePokemon.name.english}</div>
+        <div class="a-pokemon-type">${samplePokemon.type}</div>
+        <div class="a-pokemon-stats">HP:${samplePokemon.base.HP}</div>
+        <div class="a-pokemon-stats">Attack:${samplePokemon.base.Attack}</div>
+        <div class="a-pokemon-stats">Defense:${samplePokemon.base.Defense}</div>
+        <div class="a-pokemon-stats">Speed:${samplePokemon.base.Speed}</div>
+        <div class="a-pokemon-name-2">${samplePokemon.name.japanese}</div>
+        <div class="a-pokemon-name-2">${samplePokemon.name.chinese}</div>
+        <div class="a-pokemon-name-2">${samplePokemon.name.french}</div>
+    </div>
+    `
+}
 
 
-getPokemon().then(pokemon => {
-    console.log(pokemon[7])
-let samplePokemon = pokemon[0]
-document.body.innerHTML = `
-<div class="a-pokemon">
-    <div class="a-pokemon-id">${samplePokemon.id}</div>
-    <div class="a-pokemon-name">${samplePokemon.name.english}</div>
-    <div class="a-pokemon-type">${samplePokemon.type}</div>
-    <div class="a-pokemon-stats-hp">${samplePokemon.base.HP}</div>
-    <div class="a-pokemon-stats-attack">${samplePokemon.base.Attack}</div>
-    <div class="a-pokemon-stats-defense">${samplePokemon.base.Defense}</div>
-    <div class="a-pokemon-stats-speed">${samplePokemon.base.Speed}</div>
-</div>
-`
-})
+function displayPokedex(allPokemon)  {
+    let samplePokemon = allPokemon[0]
+    console.log(samplePokemon)
+    document.body.innerHTML = `<div class="my-pokedex">
+    ${allPokemon.map(samplePokemon => getPokemonHtml(samplePokemon)).join('')}
+    </div>`
+}
+getAllPokemon().then(displayPokedex)
